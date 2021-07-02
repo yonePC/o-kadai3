d53a17cbceb4048d0ea0689f8cd37b4f5ad3aee8
yonePC <ys.mailaddress0929@gmail.com>
2021-07-02 10:04:54 +0000

    [submit]応用課題2

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
@@ -17,8 +18,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(current_user.id), notice: "You have updated user successfully."
     else
       render "edit"
     end
--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :books
   attachment :profile_image, destroy: false
 
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50 }
 end
