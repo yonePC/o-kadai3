cacfa3f2a093792c201b9121b5061d3a611bfa7d
yonePC <ys.mailaddress0929@gmail.com>
2021-07-02 05:47:34 +0000

    ログイン画面の修正

--- bookers2-debug/app/controllers/application_controller.rb
+++ bookers2-debug/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!,expect: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
   end
 end
